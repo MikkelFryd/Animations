09d3e9b4fc30513fc69646422ddbad1bf87b699d
mikk7021 <82369420+mikk7021@users.noreply.github.com>
2021-08-20 12:33:44 +0200

    Add files via upload
    Animation aflevering 02

---
+++ Animation/assets/js/myscript.js
@@ -0,0 +1,12 @@
+
+function animateCats() {
+    document.getElementById("cat1").style.animationPlayState = "running";
+    document.getElementById("cat2").style.animationPlayState = "running";
+    document.getElementById("cat3").style.animationPlayState = "running";
+    document.getElementById("cat4").style.animationPlayState = "running";
+    document.getElementById("cat5").style.animationPlayState = "running";
+    document.getElementById("cat6").style.animationPlayState = "running";
+    document.getElementById("cat7").style.animationPlayState = "running";
+    document.getElementById("cat8").style.animationPlayState = "running";
+    document.getElementById("cat9").style.animationPlayState = "running";
+}
